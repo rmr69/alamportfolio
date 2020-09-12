f228bb820a1782b3a008d4cb5417a9b690d737ec
Rabiulalam <alamrobiduet074001@gmail.com>
2020-09-12 21:56:31 +0600

    All Design are Complete

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -14,6 +14,20 @@ function setActiveStyle(color){
 
 }
 
+// body Skin
+
+const bodyskin = document.querySelectorAll(".body-skin")
+    totalbodyskin = bodyskin.length;
+    for(let  i=0; i<totalbodyskin; i++){
+        bodyskin[i].addEventListener("change", function(){
+            if(this.value==="dark"){
+                document.body.className="dark";
+            }else{
+                document.body.className="";
+            }
+        })
+    }
+
 
 document.querySelector(".toggle-style-switcher").addEventListener("click",() =>{
     document.querySelector(".style-switcher").classList.toggle("open");
